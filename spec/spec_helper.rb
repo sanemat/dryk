5fdf4dd1e1055f7ecb649eefeb0b6f0d21527e38
sanemat <o.gata.ken@gmail.com>
2010-07-19 15:16:07 +0900

    use rack_dav server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,24 @@
+# coding: utf-8
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'dryk'
 require 'spec'
 require 'spec/autorun'
+require 'rack_dav'
+require 'tempfile'
 
 Spec::Runner.configure do |config|
-  
+  config.before(:each) do
+  end
 end
+
+app = Rack::Builder.new do
+  use Rack::ShowExceptions
+  use Rack::CommonLogger
+  use Rack::Reloader
+  use Rack::Lint
+  run RackDAV::Handler.new(:root => Dir.tmpdir)
+end.to_app
+
+Rack::Handler::WEBrick.run(app, :Host => '0.0.0.0', :Port => 3003)
