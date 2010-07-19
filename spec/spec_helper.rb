a6132310f5d8733355395be3ce91f03db95468c3
sanemat <o.gata.ken@gmail.com>
2010-07-19 15:46:56 +0900

    use basic auth

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ require 'tempfile'
 require 'fileutils'
 
 DOC_ROOT = File.join(Dir.tmpdir, 'dryk_9494')
+USER_NAME = 'user'
+PASSWORD = 'pass'
+
 Spec::Runner.configure do |config|
   config.before(:each) do
     FileUtils.mkdir(DOC_ROOT) unless File.exists?(DOC_ROOT)
@@ -18,10 +21,14 @@ Spec::Runner.configure do |config|
       use Rack::CommonLogger
       use Rack::Reloader
       use Rack::Lint
+      use Rack::Auth::Basic do |username, password|
+        username == USER_NAME
+        password == PASSWORD
+      end
       run RackDAV::Handler.new(:root => DOC_ROOT)
     end.to_app
 
-    Rack::Handler::WEBrick.run(app, :Host => '0.0.0.0', :Port => 9494)
+    Rack::Handler::WEBrick.run(app, :Host => 'localhost', :Port => 9494)
   end
 
   config.after(:each) do
