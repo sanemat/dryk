0df5f0a521b8f623b853e05ad782ea44bd887e41
sanemat <o.gata.ken@gmail.com>
2010-07-19 15:32:29 +0900

    each time create and delete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,18 +7,25 @@ require 'spec'
 require 'spec/autorun'
 require 'rack_dav'
 require 'tempfile'
+require 'fileutils'
 
+DOC_ROOT = File.join(Dir.tmpdir, 'dryk_11112')
 Spec::Runner.configure do |config|
   config.before(:each) do
+    FileUtils.mkdir(DOC_ROOT) unless File.exists?(DOC_ROOT)
+    app = Rack::Builder.new do
+      use Rack::ShowExceptions
+      use Rack::CommonLogger
+      use Rack::Reloader
+      use Rack::Lint
+      run RackDAV::Handler.new(:root => DOC_ROOT)
+    end.to_app
+
+    Rack::Handler::WEBrick.run(app, :Host => '0.0.0.0', :Port => 3003)
+  end
+
+  config.after(:each) do
+    FileUtils.rm_rf(DOC_ROOT) if File.exists?(DOC_ROOT)
   end
 end
 
-app = Rack::Builder.new do
-  use Rack::ShowExceptions
-  use Rack::CommonLogger
-  use Rack::Reloader
-  use Rack::Lint
-  run RackDAV::Handler.new(:root => Dir.tmpdir)
-end.to_app
-
-Rack::Handler::WEBrick.run(app, :Host => '0.0.0.0', :Port => 3003)
